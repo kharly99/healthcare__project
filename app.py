beef403fe6f271017d36aa3bbc666762d9013d2c
Eneokwe Calista <eneokwecalista@gmail.com>
2024-10-13 03:44:13 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 # Load your model file
 with open('model (2).pkl', 'rb') as f:
-    model =pickle.load("model (2).pkl")
+    model =pickle.load(f)
 
 st.title('How Age regulates Billing charges and how much patients spend for a particular medical condition predictor App')
 
